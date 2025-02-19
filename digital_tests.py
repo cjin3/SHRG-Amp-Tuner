eed6b58292f353d834d39023ec04a1d9f13b40c3
cjin3 <christopherqjin@gmail.com>
2025-02-19 14:11:18 -0500

    sine wave

--- digital_tests.py
+++ digital_tests.py
@@ -1,4 +1,19 @@
-import numpy
+import numpy as np
+import scipy.fft as fft
+import matplotlib.pyplot as plt
 
-def sine_wave():
-    
+def main():
+    sine_wave(440, 10000, 40)
+
+def sine_wave(frequency, sample_rate, samples):
+    #frequency (Hz, 1/second), sample_rate (samples/second), sample (samples)
+    sample_timer = np.linspace(0, samples/sample_rate, samples, False)
+    wave = np.sin(frequency*2*np.pi*sample_timer)
+    print(sample_timer)
+    print(wave)
+    plt.plot(sample_timer, wave)
+    plt.grid()
+    plt.show()
+    return wave
+
+main()
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import scipy
-import numpy
+import numpy as np
+import scipy.fft as fft
 import matplotlib
 import digital_tests
+
+
+def main():
+    print("hello borld")
+
+main()
